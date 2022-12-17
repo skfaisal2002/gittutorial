6babfa8f2f83f616b44dae96cc804f6780e80244
skfaisal2002 <faisalsk8484@gmail.com>
2022-12-17 11:53:00 +0530

    I AM ADDING MY FILE TO GITHUB

---
+++ Star.java
@@ -0,0 +1,12 @@
+class Star{
+    public static void main(String args[]){
+        int n=4;
+
+        for(int i=n; i<=n; i++){
+            for (int j=i; j<=i; j--){
+        System.out.print("*");
+            }
+            System.out.println();
+        }
+    } 
+}
